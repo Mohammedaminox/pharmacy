7d954b2759bc46a046193deb13ef7ef614d4ef90
Mohammed Amine <aminebenmade1999@gmial.com>
2023-12-29 16:13:08 +0100

    adding admin views and fix some bugs

--- Views/contact.php
+++ Views/contact.php
@@ -13,34 +13,34 @@
         <title>Mediplus - Free Medical and Doctor Directory HTML Template.</title>
 		
 		<!-- Favicon -->
-        <link rel="icon" href="assets/img/favicon.png">
+        <link rel="icon" href="../public/assets/img/favicon.png">
 		
 		<!-- Google Fonts -->
 		<link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
 
 		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="../public/assets/css/bootstrap.min.css">
 		<!-- Nice Select CSS -->
-		<link rel="stylesheet" href="assets/css/nice-select.css">
+		<link rel="stylesheet" href="../public/assets/css/nice-select.css">
 		<!-- Font Awesome CSS -->
-        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+        <link rel="stylesheet" href="../public/assets/css/font-awesome.min.css">
 		<!-- icofont CSS -->
-        <link rel="stylesheet" href="assets/css/icofont.css">
+        <link rel="stylesheet" href="../public/assets/css/icofont.css">
 		<!-- Slicknav -->
-		<link rel="stylesheet" href="assets/css/slicknav.min.css">
+		<link rel="stylesheet" href="../public/assets/css/slicknav.min.css">
 		<!-- Owl Carousel CSS -->
-        <link rel="stylesheet" href="assets/css/owl-carousel.css">
+        <link rel="stylesheet" href="../public/assets/css/owl-carousel.css">
 		<!-- Datepicker CSS -->
-		<link rel="stylesheet" href="assets/css/datepicker.css">
+		<link rel="stylesheet" href="../public/assets/css/datepicker.css">
 		<!-- Animate CSS -->
-        <link rel="stylesheet" href="assets/css/animate.min.css">
+        <link rel="stylesheet" href="../public/assets/css/animate.min.css">
 		<!-- Magnific Popup CSS -->
-        <link rel="stylesheet" href="assets/css/magnific-popup.css">
+        <link rel="stylesheet" href="../public/assets/css/magnific-popup.css">
 		
 		<!-- Medipro CSS -->
-        <link rel="stylesheet" href="assets/css/normalize.css">
+        <link rel="stylesheet" href="../public/assets/css/normalize.css">
         <link rel="stylesheet" href="style.css">
-        <link rel="stylesheet" href="assets/css/responsive.css">
+        <link rel="stylesheet" href="../public/assets/css/responsive.css">
 		
     </head>
     <body>
@@ -113,7 +113,7 @@
 							<div class="col-lg-3 col-md-3 col-12">
 								<!-- Start Logo -->
 								<div class="logo">
-									<a href="index.html"><img src="assets/img/logo.png" alt="#"></a>
+									<a href="index.html"><img src="../public/assets/img/logo.png" alt="#"></a>
 								</div>
 								<!-- End Logo -->
 								<!-- Mobile Nav -->
@@ -369,50 +369,50 @@
 		<!--/ End Footer Area -->
 		
 		<!-- jquery Min JS -->
-        <script src="assets/js/jquery.min.js"></script>
+        <script src="../public/assets/js/jquery.min.js"></script>
 		<!-- jquery Migrate JS -->
-		<script src="assets/js/jquery-migrate-3.0.0.js"></script>
+		<script src="../public/assets/js/jquery-migrate-3.0.0.js"></script>
 		<!-- jquery Ui JS -->
-		<script src="assets/js/jquery-ui.min.js"></script>
+		<script src="../public/assets/js/jquery-ui.min.js"></script>
 		<!-- Easing JS -->
-        <script src="assets/js/easing.js"></script>
+        <script src="../public/assets/js/easing.js"></script>
 		<!-- Color JS -->
-		<script src="assets/js/colors.js"></script>
+		<script src="../public/assets/js/colors.js"></script>
 		<!-- Popper JS -->
-		<script src="assets/js/popper.min.js"></script>
+		<script src="../public/assets/js/popper.min.js"></script>
 		<!-- Bootstrap Datepicker JS -->
-		<script src="assets/js/bootstrap-datepicker.js"></script>
+		<script src="../public/assets/js/bootstrap-datepicker.js"></script>
 		<!-- Jquery Nav JS -->
-        <script src="assets/js/jquery.nav.js"></script>
+        <script src="../public/assets/js/jquery.nav.js"></script>
 		<!-- Slicknav JS -->
-		<script src="assets/js/slicknav.min.js"></script>
+		<script src="../public/assets/js/slicknav.min.js"></script>
 		<!-- ScrollUp JS -->
-        <script src="assets/js/jquery.scrollUp.min.js"></script>
+        <script src="../public/assets/js/jquery.scrollUp.min.js"></script>
 		<!-- Niceselect JS -->
-		<script src="assets/js/niceselect.js"></script>
+		<script src="../public/assets/js/niceselect.js"></script>
 		<!-- Tilt Jquery JS -->
-		<script src="assets/js/tilt.jquery.min.js"></script>
+		<script src="../public/assets/js/tilt.jquery.min.js"></script>
 		<!-- Owl Carousel JS -->
-        <script src="assets/js/owl-carousel.js"></script>
+        <script src="../public/assets/js/owl-carousel.js"></script>
 		<!-- counterup JS -->
-		<script src="assets/js/jquery.counterup.min.js"></script>
+		<script src="../public/assets/js/jquery.counterup.min.js"></script>
 		<!-- Steller JS -->
-		<script src="assets/js/steller.js"></script>
+		<script src="../public/assets/js/steller.js"></script>
 		<!-- Wow JS -->
-		<script src="assets/js/wow.min.js"></script>
+		<script src="../public/assets/js/wow.min.js"></script>
 		<!-- Magnific Popup JS -->
-		<script src="assets/js/jquery.magnific-popup.min.js"></script>
+		<script src="../public/assets/js/jquery.magnific-popup.min.js"></script>
 		<!-- Counter Up CDN JS -->
 		<script src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
 		<!-- Google Map API Key JS -->
 		<script src="https://maps.google.com/maps/api/js?key=AIzaSyDGqTyqoPIvYxhn_Sa7ZrK5bENUWhpCo0w"></script>
 		<!-- Gmaps JS -->
-		<script src="assets/js/gmaps.min.js"></script>
+		<script src="../public/assets/js/gmaps.min.js"></script>
 		<!-- Map Active JS -->
-		<script src="assets/js/map-active.js"></script>
+		<script src="../public/assets/js/map-active.js"></script>
 		<!-- Bootstrap JS -->
-		<script src="assets/js/bootstrap.min.js"></script>
+		<script src="../public/assets/js/bootstrap.min.js"></script>
 		<!-- Main JS -->
-		<script src="assets/js/main.js"></script>
+		<script src="../public/assets/js/main.js"></script>
     </body>
 </html>
--- Views/login.php
+++ Views/login.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="assets/css/login.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/assets/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../public/assets/css/login.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/../public/assets/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
 <div class="container" id="container">
@@ -56,6 +56,6 @@
 </div>
 
   
-<script src="assets/js/login.js"></script>
+<script src="../public/assets/js/login.js"></script>
 </body>
 </html>
